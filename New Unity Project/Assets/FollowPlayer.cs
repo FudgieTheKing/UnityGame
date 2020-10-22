a998eb63d3a1f59db10354c499746309cf14fd76
FudgieTheKing <57367236+FudgieTheKing@users.noreply.github.com>
2020-10-22 02:23:11 -0400

    FirstVersion
    Put in game so we can start

---
+++ New Unity Project/Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float offsetY = 0f;
+    public float offsetZ = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, offsetY, offsetZ);
+    }
+}
---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float force = 200f;
+    public float jumpforce = 10f;
+    public bool grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void  FixedUpdate()
+    {
+        //check if on ground
+
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(force,0,0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-force, 0, 0);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            if (grounded == true)
+            {
+                rb.AddForce(0, jumpforce, 0);
+                grounded = false;
+            }
+            
+        }
+    }
+}
